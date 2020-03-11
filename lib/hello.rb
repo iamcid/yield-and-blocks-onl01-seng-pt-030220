30c25a693cfced5d7838757193444980652320fd
Christopher Cid <iamcid17@gmail.com>
2020-03-11 23:37:41 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -4,11 +4,11 @@ def hello_t(array)
     
     while i < array.length
       yield(array[i])
-      i += 1
+      i = i + 1
     end
     
     array
   else
-    puts "Hey, No block was given!"
+    puts "Hey! No block was given!"
   end
 end
